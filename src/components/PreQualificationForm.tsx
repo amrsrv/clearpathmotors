9a8b338bc2304c485cb714fc4014c876eb25fb06
amrsrv <AMIRSARAVI92@GMAIL.COM>
2025-06-10 12:01:09 -0400

    Updated PreQualificationForm.tsx

--- src/components/PreQualificationForm.tsx
+++ src/components/PreQualificationForm.tsx
@@ -1,3 +1,4 @@
+console.log('PreQualificationform is rendering')
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
