6674c5ef8e4bb0387c0bb27851c83be6e2761447
amrsrv <AMIRSARAVI92@GMAIL.COM>
2025-06-22 15:53:21 -0400

    Updated CreateAccount.tsx

--- src/pages/CreateAccount.tsx
+++ src/pages/CreateAccount.tsx
@@ -126,7 +126,7 @@ const CreateAccount = () => {
           />
         </Link>
         <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
-          Claim Your Account 
+          Claim This Loan 
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Set your password to continue
