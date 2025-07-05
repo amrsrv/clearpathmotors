2f8b35075b8c13b17b43025d95f97a0a3d70103e
amrsrv <AMIRSARAVI92@GMAIL.COM>
2025-07-04 21:20:30 -0400

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -103,6 +103,7 @@ const Navbar = () => {
                     <button
                       onClick={handleSignOut}
                       className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
                       Sign Out
                     </button>
                   </div>
