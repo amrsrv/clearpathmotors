1d436832733ac11642fc3f8ec9b15d2901ac6606
amrsrv <AMIRSARAVI92@GMAIL.COM>
2025-07-05 14:57:56 -0400

    Fix JSX syntax error in PreQualificationForm

--- src/components/PreQualificationForm.tsx
+++ src/components/PreQualificationForm.tsx
@@ -1000,7 +1000,7 @@ const PreQualificationForm: React.FC<PreQualificationFormProps> = ({ onComplete 
       {/* Sticky Footer Navigation */}
       <div className="fixed bottom-0 left-0 right-0 bg-white p-4 shadow-lg border-t border-gray-100 z-50">
         <div className="max-w-3xl mx-auto flex justify-between">
-          {currentStep > 1 ? (
+          {currentStep > 1 && (
             <>
               <Button
                 type="button"
